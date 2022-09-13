41fe48bd18d17babf2fdbb04232047e90776b94c
Ingmiguel1028 <102842224+Ingmiguel1028@users.noreply.github.com>
2022-09-13 16:12:55 -0500

    instruciones de pruebas unitarias

---
+++ unit_test_1.py
@@ -0,0 +1,10 @@
+
+import unittest
+
+from Inference import preprocess_img
+
+#Se encarga de comprobar el preprocesado  de imagenes
+
+class unit_test_1(unittest.TestCase):
+    def unit_test_1(self):
+        preprocess_img.App.preprocess(self)
---
+++ unit_test_2.py
@@ -0,0 +1,10 @@
+
+import unittest
+
+from Backend import read_img
+
+#Se encarga de comprobar la correcta carga de imagenes
+
+class unit_test_2(unittest.TestCase):
+    def unit_test_2(self):
+        read_img.App.read_jpg_file(self)
